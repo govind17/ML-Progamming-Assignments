0b0dc1e67580af279d1d99b305dc40db71549dfc
Govind <govindshukla17@gmail.com>
2020-12-22 19:50:32 +0530

    Complete program with output

--- Neural Networks/NN_Sigmoid.py
+++ Neural Networks/NN_Sigmoid.py
@@ -1,86 +1,57 @@
 import argparse
 import csv
 import numpy as np
-from random import random
 
-# Initialize a network
-def initialize_network():
-	network = list()
-	hidden_layer = [{'weights': [0.2,-0.3,0.4]}, {'weights': [-0.5,-0.1,-0.4]},{'weights': [0.3,0.2,0.1]}]
-	network.append(hidden_layer)
-	output_layer = [{'weights':[-0.1,0.1,0.3,-0.4]}]
-	network.append(output_layer)
-	return network
+def sigmoidNeuralNetworks(file_path, learningrate, iterations):
+    data = np.genfromtxt(file_path, delimiter=',')
+    #Initializing the weights
+    w_bias_h1,w_a_h1,w_b_h1,w_bias_h2,w_a_h2,w_b_h2,w_bias_h3,w_a_h3,w_b_h3,w_bias_o,w_h1_o,w_h2_o,w_h3_o,eta= 0.2,-0.3,0.4,-0.5,-0.1,-0.4,0.3,0.2,0.1,-0.1,0.1,0.3,-0.4,learningrate;
+    i=0
+    #Printing the hardcoded values of first 2 lines of the output
+    print('a','b','h1','h2','h3','o','t','delta_h1','delta_h2','delta_h3','delta_o','w_bias_h1','w_a_h1','w_b_h1','w_bias_h2','w_a_h2','w_b_h2','w_bias_h3','w_a_h3','w_b_h3','w_bias_o','w_h1_o','w_h2_o','w_h3_o',sep=',')
+    print('-','-','-','-','-','-','-','-','-','-','-',w_bias_h1,w_a_h1,w_b_h1,w_bias_h2,w_a_h2,w_b_h2,w_bias_h3,w_a_h3,w_b_h3,w_bias_o,w_h1_o,w_h2_o,w_h3_o,sep=',')
+    while i<iterations:
+      
+      for row in data:
+        Input_a=row[0]
+        Input_b = row[1]
+        #Calculating weighted sum at the hidden layer
+        net_h1 = w_a_h1 * Input_a + w_b_h1 * Input_b + w_bias_h1 * 1
+        net_h2 = w_a_h2 * Input_a + w_b_h2 * Input_b + w_bias_h2 * 1
+        net_h3 = w_a_h3 * Input_a + w_b_h3 * Input_b + w_bias_h3 * 1
+        #Calculating the output through each neuron using sigmoid activation function
+        sigmoidActivation_h1 = 1 / (1 + np.exp(-net_h1))
+        sigmoidActivation_h2 = 1 / (1 + np.exp(-net_h2))
+        sigmoidActivation_h3 = 1 / (1 + np.exp(-net_h3))
+        net_output = sigmoidActivation_h1 * w_h1_o + sigmoidActivation_h2 * w_h2_o + sigmoidActivation_h3 * w_h3_o + 1 * w_bias_o
+        sigmoidActivation_output = 1 / (1 + np.exp(-net_output))  #Net output
+        error = row[2] - sigmoidActivation_output  #Error in output and actual prediction
+        if error:
+            #Calclating error at hidden layer
+            delta_o = sigmoidActivation_output*(1-sigmoidActivation_output)*(row[2]-sigmoidActivation_output)
+            delta_h1= sigmoidActivation_h1*(1-sigmoidActivation_h1)*(w_h1_o*delta_o)
+            delta_h2= sigmoidActivation_h2 * (1 - sigmoidActivation_h2) * (w_h2_o  * delta_o)
+            delta_h3= sigmoidActivation_h3 * (1 - sigmoidActivation_h3) * (w_h3_o * delta_o)
+            #Updating the weights at hidden layer
+            w_bias_h1= w_bias_h1+(eta * delta_h1 * 1)
+            w_a_h1 = w_a_h1+(eta * delta_h1 * Input_a)
+            w_b_h1 = w_b_h1+(eta * delta_h1 * Input_b)
+            w_bias_h2 = w_bias_h2+(eta * delta_h2 * 1)
+            w_a_h2 = w_a_h2+(eta * delta_h2 * Input_a)
+            w_b_h2 = w_b_h2+(eta * delta_h2 * Input_b)
+            w_bias_h3 = w_bias_h3+(eta * delta_h3 * 1)
+            w_a_h3 = w_a_h3+(eta * delta_h3 * Input_a)
+            w_b_h3 = w_b_h3+(eta * delta_h3 * Input_b)
+            #Updaing the weights at output layer
+            w_bias_o = w_bias_o+(eta * delta_o * 1)
+            w_h1_o = w_h1_o+(eta * delta_o * sigmoidActivation_h1)
+            w_h2_o = w_h2_o+(eta * delta_o * sigmoidActivation_h2)
+            w_h3_o = w_h3_o+(eta * delta_o * sigmoidActivation_h3)
+            arr=np.array((row[0],row[1],sigmoidActivation_h1,sigmoidActivation_h2,sigmoidActivation_h3,sigmoidActivation_output,row[2],delta_h1,delta_h2,delta_h3,delta_o,w_bias_h1,w_a_h1,w_b_h1,w_bias_h2,w_a_h2,w_b_h2,w_bias_h3,w_a_h3,w_b_h3,w_bias_o,w_h1_o,w_h2_o,w_h3_o))
+            arr1 = np.round(arr,5)
+            print(*list(arr1),sep=',')
 
-# Calculate neuron activation for an input
-def activate(weights, inputs):
-	activation = weights[-1]
-	for i in range(len(weights)-1):
-		activation += weights[i] * float(inputs[i])
-	return activation
-
-# Transfer neuron activation(Sigmoid)
-def transfer(activation):
-	return 1.0 / (1.0 + np.exp(-activation))
-
-# Forward propagate input to a network output
-def forward_propagate(network, row):
-	inputs = row
-	for layer in network:
-		new_inputs = []
-		for neuron in layer:
-			activation = activate(neuron['weights'], inputs)
-			neuron['output'] = transfer(activation)
-			new_inputs.append(neuron['output'])
-		inputs = new_inputs
-	return inputs
-
-# Calculate the derivative of an neuron output
-def transfer_derivative(output):
-	return output * (1.0 - output)
-
-# Backpropagate error and store in neurons
-def backward_propagate_error(network, expected):
-	for i in reversed(range(len(network))):
-		layer = network[i]
-		errors = list()
-		if i != len(network)-1:
-			for j in range(len(layer)):
-				error = 0.0
-				for neuron in network[i + 1]:
-					error += (neuron['weights'][j] * neuron['delta'])
-				errors.append(error)
-		else:
-			for j in range(len(layer)):
-				neuron = layer[j]
-				errors.append(expected[j] - neuron['output'])
-		for j in range(len(layer)):
-			neuron = layer[j]
-			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
-
-# Update network weights with error
-def update_weights(network, row, l_rate):
-	for i in range(len(network)):
-		inputs = row[:-1]
-		if i != 0:
-			inputs = [neuron['output'] for neuron in network[i - 1]]
-		for neuron in network[i]:
-			for j in range(len(inputs)):
-				neuron['weights'][j] += l_rate * neuron['delta'] * float(inputs[j])
-			neuron['weights'][-1] += l_rate * neuron['delta']
-
-# Train a network for a fixed number of epochs
-def train_network(network, train, l_rate, n_epoch, n_outputs):
-	for epoch in range(n_epoch):
-		sum_error = 0
-		for row in train:
-			outputs = forward_propagate(network, row)
-			expected = [0 for i in range(n_outputs)]
-			expected[int(row[-1])] = 1
-			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
-			backward_propagate_error(network, expected)
-			update_weights(network, row, l_rate)
-		#print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+      i+=1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -88,15 +59,5 @@ if __name__ == "__main__":
     parser.add_argument("--eta", type=float)
     parser.add_argument("--iterations", type=int)
     args = parser.parse_args()
-    file_path, learningRate, itr = args.data, args.eta, args.iterations    #Reading the arguments
-    with open(file_path) as dataFile:
-        csv_reader = csv.reader(dataFile, delimiter=',')
-        dataset=list(csv_reader)
-        n_inputs = len(dataset[0]) - 1
-        n_outputs = len(set([row[-1] for row in dataset]))
-        network = initialize_network()
-        train_network(network, dataset, 0.2, itr, n_outputs)
-        
-        '''for layer in network:
-	        print(layer)'''
-
+    file_path, learningRate, iterations = args.data, args.eta, args.iterations    #Reading the arguments
+    sigmoidNeuralNetworks(file_path,learningRate,iterations)
